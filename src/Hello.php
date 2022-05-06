b24e9c2e70aa6c689b4814ecdc9077f252356e01
root <colliezc@miamioh.edu>
2022-05-06 17:42:45 -0400

    Created hello.php

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Zack Collier <colliezc@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Zackcollier\WeatherPackage;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
